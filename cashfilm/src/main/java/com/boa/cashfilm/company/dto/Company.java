f28355099a5148e369ae77762815699f958ec6e8
황신독 <@sindock>
2018-04-05 11:28:05 +0900

    (황신독) 정보조회,등록dto

--- cashfilm/src/main/java/com/boa/cashfilm/company/dto/Company.java
+++ cashfilm/src/main/java/com/boa/cashfilm/company/dto/Company.java
@@ -1,5 +1,172 @@
 package com.boa.cashfilm.company.dto;
 
 public class Company {
+	private int comCode;
+	private String comName;	
+	private String comRegistrationNumber;
+	private String comWebPage;
+	private String comCeoName;
+	private String comInfo;
+	private String comLogoName;
+	private String comLogoOriginalName;
+	private int comLogoHeight;
+	private int comLogoWidth;
+	private int comLogoFileSize;
+	private String comLogoExtension;
+	private String comIntroduction;
+	private String comAddress;
+	private String comPhone;
+	private String memberEmail;
+	private String comRegistrationRequestDay;
+	private int managerApproval;
+	private String managerApprovalDay;
+	private String managerApprovalEmail;
+	private String comIndustry;
+	private String comCondition;
+	public int getComCode() {
+		return comCode;
+	}
+	public void setComCode(int comCode) {
+		this.comCode = comCode;
+	}
+	public String getComName() {
+		return comName;
+	}
+	public void setComName(String comName) {
+		this.comName = comName;
+	}
+	public String getComRegistrationNumber() {
+		return comRegistrationNumber;
+	}
+	public void setComRegistrationNumber(String comRegistrationNumber) {
+		this.comRegistrationNumber = comRegistrationNumber;
+	}
+	public String getComWebPage() {
+		return comWebPage;
+	}
+	public void setComWebPage(String comWebPage) {
+		this.comWebPage = comWebPage;
+	}
+	public String getComCeoName() {
+		return comCeoName;
+	}
+	public void setComCeoName(String comCeoName) {
+		this.comCeoName = comCeoName;
+	}
+	public String getComInfo() {
+		return comInfo;
+	}
+	public void setComInfo(String comInfo) {
+		this.comInfo = comInfo;
+	}
+	public String getComLogoName() {
+		return comLogoName;
+	}
+	public void setComLogoName(String comLogoName) {
+		this.comLogoName = comLogoName;
+	}
+	public String getComLogoOriginalName() {
+		return comLogoOriginalName;
+	}
+	public void setComLogoOriginalName(String comLogoOriginalName) {
+		this.comLogoOriginalName = comLogoOriginalName;
+	}
+	public int getComLogoHeight() {
+		return comLogoHeight;
+	}
+	public void setComLogoHeight(int comLogoHeight) {
+		this.comLogoHeight = comLogoHeight;
+	}
+	public int getComLogoWidth() {
+		return comLogoWidth;
+	}
+	public void setComLogoWidth(int comLogoWidth) {
+		this.comLogoWidth = comLogoWidth;
+	}
+	public int getComLogoFileSize() {
+		return comLogoFileSize;
+	}
+	public void setComLogoFileSize(int comLogoFileSize) {
+		this.comLogoFileSize = comLogoFileSize;
+	}
+	public String getComLogoExtension() {
+		return comLogoExtension;
+	}
+	public void setComLogoExtension(String comLogoExtension) {
+		this.comLogoExtension = comLogoExtension;
+	}
+	public String getComIntroduction() {
+		return comIntroduction;
+	}
+	public void setComIntroduction(String comIntroduction) {
+		this.comIntroduction = comIntroduction;
+	}
+	public String getComAddress() {
+		return comAddress;
+	}
+	public void setComAddress(String comAddress) {
+		this.comAddress = comAddress;
+	}
+	public String getComPhone() {
+		return comPhone;
+	}
+	public void setComPhone(String comPhone) {
+		this.comPhone = comPhone;
+	}
+	public String getMemberEmail() {
+		return memberEmail;
+	}
+	public void setMemberEmail(String memberEmail) {
+		this.memberEmail = memberEmail;
+	}
+	public String getComRegistrationRequestDay() {
+		return comRegistrationRequestDay;
+	}
+	public void setComRegistrationRequestDay(String comRegistrationRequestDay) {
+		this.comRegistrationRequestDay = comRegistrationRequestDay;
+	}
+	public int getManagerApproval() {
+		return managerApproval;
+	}
+	public void setManagerApproval(int managerApproval) {
+		this.managerApproval = managerApproval;
+	}
+	public String getManagerApprovalDay() {
+		return managerApprovalDay;
+	}
+	public void setManagerApprovalDay(String managerApprovalDay) {
+		this.managerApprovalDay = managerApprovalDay;
+	}
+	public String getManagerApprovalEmail() {
+		return managerApprovalEmail;
+	}
+	public void setManagerApprovalEmail(String managerApprovalEmail) {
+		this.managerApprovalEmail = managerApprovalEmail;
+	}
+	public String getComIndustry() {
+		return comIndustry;
+	}
+	public void setComIndustry(String comIndustry) {
+		this.comIndustry = comIndustry;
+	}
+	public String getComCondition() {
+		return comCondition;
+	}
+	public void setComCondition(String comCondition) {
+		this.comCondition = comCondition;
+	}
+	@Override
+	public String toString() {
+		return "Company [comCode=" + comCode + ", comName=" + comName + ", comRegistrationNumber="
+				+ comRegistrationNumber + ", comWebPage=" + comWebPage + ", comCeoName=" + comCeoName + ", comInfo="
+				+ comInfo + ", comLogoName=" + comLogoName + ", comLogoOriginalName=" + comLogoOriginalName
+				+ ", comLogoHeight=" + comLogoHeight + ", comLogoWidth=" + comLogoWidth + ", comLogoFileSize="
+				+ comLogoFileSize + ", comLogoExtension=" + comLogoExtension + ", comIntroduction=" + comIntroduction
+				+ ", comAddress=" + comAddress + ", comPhone=" + comPhone + ", memberEmail=" + memberEmail
+				+ ", comRegistrationRequestDay=" + comRegistrationRequestDay + ", managerApproval=" + managerApproval
+				+ ", managerApprovalDay=" + managerApprovalDay + ", managerApprovalEmail=" + managerApprovalEmail
+				+ ", comIndustry=" + comIndustry + ", comCondition=" + comCondition + "]";
+	}
 
+	
 }
