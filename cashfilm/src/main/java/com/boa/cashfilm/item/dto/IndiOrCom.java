0ec70da0a2634b964c7d3239fd77e366e02ed9a7
안영신 <@DESKTOP-UQUOB9R>
2018-04-09 10:13:14 +0900

    (안영신) 회원들 상품구매를 위해 Dto 추가

---
+++ cashfilm/src/main/java/com/boa/cashfilm/item/dto/IndiOrCom.java
@@ -0,0 +1,17 @@
+package com.boa.cashfilm.item.dto;
+
+public class IndiOrCom {
+	private int indiOrCom;
+
+	public int getIndiOrCom() {
+		return indiOrCom;
+	}
+	public void setIndiOrCom(int indiOrCom) {
+		this.indiOrCom = indiOrCom;
+	}
+
+	@Override
+	public String toString() {
+		return "IndiOrCom [indiOrCom=" + indiOrCom + "]";
+	}
+}
