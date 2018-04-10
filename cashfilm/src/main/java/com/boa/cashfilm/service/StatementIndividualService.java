5e9bbf69072cd89886d1cc25fbad5482ee1c0892
jwliu <jwliu@DESKTOP-R83NNRK>
2018-04-10 11:54:45 +0900

    (이원진)개인전표관리 셋팅

--- cashfilm/src/main/java/com/boa/cashfilm/service/StatementIndividualService.java
+++ cashfilm/src/main/java/com/boa/cashfilm/service/StatementIndividualService.java
@@ -1,5 +1,20 @@
 package com.boa.cashfilm.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.boa.cashfilm.dao.StatementIndividualDao;
+
+
+
+@Service
+@Transactional
 public class StatementIndividualService {
+	@Autowired
+	private StatementIndividualDao systemindIvidualDao;
+	private static final Logger logger = LoggerFactory.getLogger(StatementIndividualService.class);
 
 }
